d23c52c313ecb27e6d797927f1a2d15e42f6ae83
Sunny Rekhi <rohit.rekhi@gmail.com>
2016-06-19 22:09:54 -0500

    user/contact model add/update

--- network_tool/db/schema.rb
+++ network_tool/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160619155238) do
+ActiveRecord::Schema.define(version: 20160619164431) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.string   "email"
+    t.text     "information"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
