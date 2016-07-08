8a12e6ff06b6b1a0e97f7735f434b4f4a00cc132
Sunny Rekhi <rohit.rekhi@gmail.com>
2016-07-08 12:59:12 -0500

    going thru sidekiq wiki and adding accordingly

--- network_tool/app/workers/reminders_worker.rb
+++ network_tool/app/workers/reminders_worker.rb
@@ -1,12 +1,14 @@
 class RemindersWorker
 
-  def perform(name, count)
-  	Reminder.mail
-    # do something
-    #This is where job done that needs to be done in background
+ def perform(reminder, number)
+  	client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+  	message = client.messages.create from: '8326482121', to: 'number', body: 'Hi, This is a reminder to text @reminder.contact regarding @reminder.occasion.'
+  	
+    ##Do something later
+  end
    
 #THE BELOW IS HOW YOU CALL THIS METHOD
     #RemindersWorker.perform_async
     # https://www.youtube.com/watch?v=iIXLt24f8Mg
-  end
+  
 end
