07a3595ec412c18b9d9729c89d223ca48a78bd0a
Sunny Rekhi <rohit.rekhi@gmail.com>
2016-07-12 08:04:08 -0500

    added client side validation gem

--- network_tool/config/initializers/session_store.rb
+++ network_tool/config/initializers/session_store.rb
@@ -1,5 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
+
 Rails.application.config.session_store :cookie_store, key: '_network_tool_session'
-NetworkTool::Application.config.session_store :redis_store, servers: "redis://localhost:6379/0/session"
+
+#REMOVING THE BELOW LINE ACCORDING TO AN SO POST. THIS IS A TEST. SEEMS TO HAVE SUCCEDED.
+#NetworkTool::Application.config.session_store :redis_store, servers: "redis://localhost:6379/0/session"
 
