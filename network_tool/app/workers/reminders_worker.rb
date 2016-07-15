52134c8daa5d4b15df074c806f413118b0479035
Sunny Rekhi <rohit.rekhi@gmail.com>
2016-07-15 08:41:53 -0500

    continuing debugging workers file

--- network_tool/app/workers/reminders_worker.rb
+++ network_tool/app/workers/reminders_worker.rb
@@ -1,9 +1,9 @@
 require 'sidekiq'
 require 'celluloid/current'
 
-
 class RemindersWorker
 	include Sidekiq::Worker
+  	include Sidetiq::Schedulable
 	include Sidekiq::Worker
 
 	 def perform(reminder_id, number, recurring)
@@ -14,7 +14,6 @@ class RemindersWorker
 	 		end
 	 	end
 	 	if recurring.start_with('Not') #hardcoding this now will come back and improve.
-	 		break
 	 	else if recurring == '1 month'
 	 		recurrence {monthly}
 	 		#recurrence do 
@@ -28,8 +27,6 @@ class RemindersWorker
 	 	else if recurring == '1 year'
 	 		recurrence {yearly}
 	 	end
-
-
 	  	client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
 	  	message = client.messages.create from: '8326482121', to: number, body: "Hi, This is a reminder to text #{contact.name} regarding their #{reminder.occasion} today!"
   	end
@@ -37,5 +34,4 @@ class RemindersWorker
 #THE BELOW IS HOW YOU CALL THIS METHOD
     #RemindersWorker.perform_async
     # https://www.youtube.com/watch?v=iIXLt24f8Mg
-  
 end
