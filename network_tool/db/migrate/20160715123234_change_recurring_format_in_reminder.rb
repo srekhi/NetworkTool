0216ac1bf5d85d3bdd18ddceab7d8555ed59966a
Sunny Rekhi <rohit.rekhi@gmail.com>
2016-07-20 09:47:42 -0500

    added pg workout for for data type changes

--- network_tool/db/migrate/20160715123234_change_recurring_format_in_reminder.rb
+++ network_tool/db/migrate/20160715123234_change_recurring_format_in_reminder.rb
@@ -1,9 +1,13 @@
 class ChangeRecurringFormatInReminder < ActiveRecord::Migration
   def up
-    change_column :reminders, :recurring, :integer
+    #change_column :reminders, :recurring, :integer
+    execute 'ALTER TABLE reminders ALTER COLUMN recurring TYPE boolean USING (recurring::integer)'
+
   end
 
   def down
-    change_column :reminders, :recurring, :boolean
+    #change_column :reminders, :recurring, :boolean
+    execute 'ALTER TABLE reminders ALTER COLUMN recurring TYPE boolean USING (recurring::boolean)'
+
   end
 end
--- network_tool/db/migrate/20160715124917_change_recurring_format_in_reminder_again.rb
+++ network_tool/db/migrate/20160715124917_change_recurring_format_in_reminder_again.rb
@@ -1,9 +1,13 @@
 class ChangeRecurringFormatInReminderAgain < ActiveRecord::Migration
   def up
     change_column :reminders, :recurring, :string
+    execute 'ALTER TABLE reminders ALTER COLUMN recurring TYPE string USING (recurring::string)'
+
   end
 
   def down
     change_column :reminders, :recurring, :integer
+    execute 'ALTER TABLE reminders ALTER COLUMN recurring TYPE integer USING (recurring::integer)'
+
   end
 end
