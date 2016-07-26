71091a1747ebaec2fecd616b930b12460db4bf35
Sunny Rekhi <rohit.rekhi@gmail.com>
2016-07-26 12:22:06 -0500

    editing add_contact_spec to get it working...still nothing yet

--- network_tool/spec/features/add_contact_spec.rb
+++ network_tool/spec/features/add_contact_spec.rb
@@ -7,25 +7,41 @@ RSpec.describe "adding a contact", type: :feature do
      	@before_count = Contact.count
 	    @user = FactoryGirl.build(:user)
 	    @contact = FactoryGirl.build(:contact)
+
+	     visit new_user_registration_path
+		 fill_in 'Email', with: @user.email
+		 fill_in 'Phone number', with: @user.phone_number
+		 fill_in 'Password', with: @user.password
+		 fill_in 'Password confirmation', with: @user.password
+		 click_button "Sign up"
+		    #binding.pry
+
+		 click_button "New Contact"
+
+		    #hardcoding above to simulate user sign up. Will refactor later.
+		 fill_in 'name', with: @contact.name
+		 fill_in 'email', with: @contact.email
+		 fill_in 'information', with: @contact.information
+		 click_button "Create Contact"
 	 end
 
 	    
-	    it "registers user and adds a contact", :js => true do
-		    visit new_user_registration_path
-		    fill_in 'Email', with: @user.email
-		    fill_in 'Phone number', with: @user.phone_number
-		    fill_in 'Password', with: @user.password
-		    fill_in 'Password confirmation', with: @user.password
-		    click_button "Sign up"
-		    binding.pry
+	   # it "registers user and adds a contact" do
+		#    visit new_user_registration_path
+		 #   fill_in 'Email', with: @user.email
+		  #  fill_in 'Phone number', with: @user.phone_number
+		   # fill_in 'Password', with: @user.password
+		    #fill_in 'Password confirmation', with: @user.password
+		    #click_button "Sign up"
+		    #binding.pry
 
-		    click_button "New Contact"
+		    #click_button "New Contact"
 
 		    #hardcoding above to simulate user sign up. Will refactor later.
-		    fill_in 'name', with: @contact.name
-		    fill_in 'email', with: @contact.email
-		    fill_in 'information', with: @contact.information
-		end
+		    #fill_in 'name', with: @contact.name
+		    #fill_in 'email', with: @contact.email
+		    #fill_in 'information', with: @contact.information
+		#end
 	
 	  it "added a contact in the rails database" do
 		  expect(Contact.count).not_to eq(@before_count)
